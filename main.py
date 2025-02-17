c7b17fcf02c26999c6cc96fef536cd1d2f2c3295
pablitotechi <105807452+PablitoGarro@users.noreply.github.com>
2025-02-17 14:50:16 -0600

    Mensaje descriptivo sobre lo que has cambiado

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Lista donde almacenaremos los usuarios (en este caso solo en memoria)
+users = []
+
+# Definir un modelo para el usuario
+class User(BaseModel):
+    name: str
+    email: str
+
+# API POST para crear un usuario
+@app.post("/user/")
+async def create_user(user: User):
+    users.append(user)
+    return {"message": "User created successfully", "user": user}
+
+# API GET para obtener el usuario
+@app.get("/user/")
+async def get_user():
+    if not users:
+        return {"message": "No users found"}
+    return {"users": users}
